ca29808f4136fe39905df588143609303cc234fd
Sten Verbois <stenverbois@gmail.com>
2018-06-18 13:21:59 -0700

    Update protocol

--- enclaves/attestation-server/enclave/src/lib.rs
+++ enclaves/attestation-server/enclave/src/lib.rs
@@ -68,42 +68,46 @@ lazy_static! {
 struct Key([u8; SANCUS_KEY_SIZE]);
 
 /// Returns a tuple containing the key distribution sequence and the expected nonce mac.
-fn build_key_distribution_sequence(key_pm: &SancusKey, key_conn: &SancusKey, id_conn: u16) -> (impl Iterator<Item=CANPayload>, CANPayload) {
-    let mut sequence: [CANPayload; 6] = [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]; 6];
+fn build_key_distribution_sequence(id_pm: u16, key_pm: &SancusKey, id_conn: u16, key_conn: &SancusKey) -> (impl Iterator<Item=CANPayload>, CANPayload) {
+    let mut sequence: [CANPayload; 5] = [[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]; 5];
 
-    let nonce = sgx_rand::random::<u64>();
-    println!("Generated nonce {:x}", nonce);
+    let mut id_pm_buf = [0; 2];
+    LittleEndian::write_u16(&mut id_pm_buf, id_pm);
 
-    let mut nonce_buf = [0; 8];
-    BigEndian::write_u64(&mut nonce_buf, nonce);
+    let mut id_conn_buf = [0; 2];
+    LittleEndian::write_u16(&mut id_conn_buf, id_conn);
 
-    let mut id_buf = [0; 2];
-    LittleEndian::write_u16(&mut id_buf, id_conn);
-
+    println!("Connection key: ");
     for &byte in key_conn.iter() {
         print!("{:02x}", byte);
     }
     println!("");
 
-    let mut payload = [0x00; 8 * 4];
-    payload[..2].clone_from_slice(&id_buf);
-    payload[8..16].clone_from_slice(&nonce_buf);
-    payload[16..].clone_from_slice(key_conn);
+    let mut payload = [0x00; 8 * 3];
+    payload[..2].clone_from_slice(&id_pm_buf);
+    payload[2..4].clone_from_slice(&id_conn_buf);
+    payload[8..].clone_from_slice(key_conn);
 
-    let mut payload_encrypted = [0x00; 8 * 4];
-    let mac = spongent_wrap(key_pm, &[0x01, 0x02, 0x03, 0x04], &payload, &mut payload_encrypted, false).unwrap();
+    let mut payload_encrypted = [0x00; 8 * 3];
+    let mac = spongent_wrap(key_pm, &[0x00, 0x00, 0x00, 0x00], &payload, &mut payload_encrypted, false).unwrap();
 
     // TODO impl Default?
     let mut expected_nonce_mac: CANPayload = [0x00; CAN_PAYLOAD_SIZE];
-    expected_nonce_mac.clone_from_slice(&spongent_mac(key_pm, &nonce_buf).unwrap()[8..]);
+    let x = spongent_mac(key_conn, &[0xA7, 0x7E, 0x57, 0xED]).unwrap();
+    expected_nonce_mac.clone_from_slice(&spongent_mac(key_conn, &[0xA7, 0x7E, 0x57, 0xED]).unwrap()[8..]);
+
+    print!("Expected calced for {:?}-{:?}: ", id_pm, id_conn);
+    for &byte in &x {
+        print!("{:02x}", byte);
+    }
+    println!("");
 
     sequence[0].copy_from_slice(&payload_encrypted[0..8]);
     sequence[1].copy_from_slice(&payload_encrypted[8..16]);
     sequence[2].copy_from_slice(&payload_encrypted[16..24]);
-    sequence[3].copy_from_slice(&payload_encrypted[24..]);
     
-    sequence[4].copy_from_slice(&mac[..8]);
-    sequence[5].copy_from_slice(&mac[8..]);
+    sequence[3].copy_from_slice(&mac[..8]);
+    sequence[4].copy_from_slice(&mac[8..]);
 
     (sequence.to_vec().into_iter(), expected_nonce_mac)
 }
@@ -133,8 +137,8 @@ pub extern "C" fn initialize() -> u32 {
     // TODO Should be retrieved from confidentiallity and integrity protected
     // storage
     let mut pm_keys = PM_KEYS.lock().unwrap();
-    pm_keys.insert(0x01, [0xab, 0x30, 0x62, 0x72, 0x34, 0xd6, 0x77, 0x01, 0x3b, 0x0f, 0xbb, 0xe6, 0xc9, 0x0a, 0xcb, 0x2a]);
-    pm_keys.insert(0x02, [0xa5, 0xf3, 0x9b, 0x2a, 0x13, 0xd7, 0x58, 0x29, 0x44, 0x62, 0x8e, 0x1d, 0xe5, 0x1f, 0x53, 0x08]);
+    pm_keys.insert(0x01, [0x3d, 0x52, 0xa7, 0x75, 0x27, 0x98, 0xb7, 0xed, 0x83, 0xb5, 0xf9, 0x0b, 0x70, 0x83, 0x2c, 0x4a]);
+    pm_keys.insert(0x02, [0x38, 0x2b, 0xa6, 0x3f, 0xd3, 0x85, 0x70, 0xfa, 0x1c, 0xfa, 0x43, 0xf7, 0x99, 0x1b, 0xd7, 0xf6]);
 
     // Randomly generate connection keys for all connections
     let mut connection_keys = CONNECTION_KEYS.lock().unwrap();
@@ -166,7 +170,7 @@ pub extern "C" fn initialize() -> u32 {
         for id_conn in connections {
             let key_conn = connection_keys.get(&id_conn).expect("Missing connection key");
 
-            let (sequence, expected_nonce_mac) = build_key_distribution_sequence(&key_pm, &key_conn, *id_conn);
+            let (sequence, expected_nonce_mac) = build_key_distribution_sequence(id_pm, &key_pm, *id_conn, &key_conn);
             nonce_macs.get_mut(&id_pm).unwrap().push((*id_conn, expected_nonce_mac));
             for msg in sequence {
                 vulcan_send(CAN_ID_ATTEST_SEND as u32, &msg);
@@ -212,6 +216,19 @@ pub extern "C" fn recv_message(eid: u32, dlen: u32, data: *const u8) -> u16 {
                         return 0;
                     }
                 };
+
+                println!("expecting: ");
+                for &byte in &nonce_macs.get_mut(&pm_id).unwrap().get(pos).unwrap().1 {
+                    print!("{:02x}", byte);
+                }
+                println!("");
+
+                if nonce_macs.get_mut(&pm_id).unwrap().get(pos).unwrap().1 == data {
+                    println!("MACs match");
+                }
+                else {
+                    println!("MACs don't match");
+                }
 
                 nonce_macs.get_mut(&pm_id).unwrap().remove(pos);
                 *expect_mac = None;
